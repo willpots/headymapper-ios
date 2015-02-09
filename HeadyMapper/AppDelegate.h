da641717d0ab774d87dfcd9a4cdf59a3cf69ec97
Will Potter <willstonepotter@gmail.com>
2015-02-09 14:17:44 -0500

    Initial Commit

---
+++ HeadyMapper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HeadyMapper
+//
+//  Created by Will Potter on 2/9/15.
+//  Copyright (c) 2015 Will Potter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HeadyMapper/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  HeadyMapper
+//
+//  Created by Will Potter on 2/9/15.
+//  Copyright (c) 2015 Will Potter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
